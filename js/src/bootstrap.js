17e7fd463508be77eef2b47bbe0ddf844a7e3657
jiajun <jiajun@jiajundeMacBook-Pro.local>
2019-07-14 00:52:24 +0800

    Site updated: 2019-07-14 00:52:23

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -8,32 +8,10 @@ $(document).ready(function () {
 
   NexT.utils.lazyLoadPostsImages();
 
-  NexT.utils.registerBackToTop();
-
-  $('.site-nav-toggle button').on('click', function () {
-    var $siteNav = $('.site-nav');
-    var ON_CLASS_NAME = 'site-nav-on';
-    var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
-    var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
-    var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
-
-    $siteNav.stop()[animateAction]('fast', function () {
-      $siteNav[animateCallback](ON_CLASS_NAME);
-    });
-  });
-
-
-  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  NexT.utils.embeddedVideoTransformer();
-  NexT.utils.addActiveClassToMenuItem();
-
-
   // Define Motion Sequence.
   NexT.motion.integrator
     .add(NexT.motion.middleWares.logo)
-    .add(NexT.motion.middleWares.menu)
-    .add(NexT.motion.middleWares.postList)
-    .add(NexT.motion.middleWares.sidebar);
+    .add(NexT.motion.middleWares.postList);
 
   $(document).trigger('motion:before');
 
